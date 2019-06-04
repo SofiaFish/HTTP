30c7b36ef50549aa135d32aa230f6caa23a16484
SofiaFish <37893374+SofiaFish@users.noreply.github.com>
2019-06-04 14:13:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace HttpRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endPoint = "https://google.com";
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(endPoint);
+            request.Method = "GET";
+            request.UserAgent = "browser";
+            request.Headers.Add("Content-Type", "application/json");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            Console.WriteLine(reader.ReadToEnd());
+            response.Close();
+            reader.Close();
+
+        }
+    }
+}
